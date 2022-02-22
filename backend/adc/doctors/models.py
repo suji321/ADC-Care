1db893627706fead4bebd4ee363ddd8287eb1771
aleena-james <aleena.jms1099@gmail.com>
2022-02-23 01:04:46 +0530

    doc model

--- backend/adc/doctors/models.py
+++ backend/adc/doctors/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Doctors(models.Model):
+    dname = models.CharField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    role = models.CharField(max_length=200, null=True)
+    specialisation = models.CharField(max_length=200, null=True)
+    address= models.CharField(max_length=200, null=True)
