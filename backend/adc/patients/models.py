e4cc21207614a1244f1f92ebaefdf0f3dcdbece1
aleena-james <aleena.jms1099@gmail.com>
2022-02-23 23:42:12 +0530

    patient model

--- backend/adc/patients/models.py
+++ backend/adc/patients/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    pname=models.CharField(max_length=200, null=True)
+    age=models.CharField(max_length=4, null=True)
+    gender= models.CharField(max_length=6, null=True)
+    email=models.CharField(max_length=200, null=True)
+    address= models.CharField(max_length=500, null=True)
+    phone= models.CharField(max_length=13, null=True)
+    # medicalHistory: models.CharField(max_length=200, null=True)
+    date_created= models.DateTimeField(auto_now_add=True, null=True)
