f5f361ae4df5e0bc22cd4c2921e578165f1b95b0
aleena-james <aleena.jms1099@gmail.com>
2022-02-24 00:51:41 +0530

    bill

--- backend/adc/clinic/admin.py
+++ backend/adc/clinic/admin.py
@@ -4,3 +4,4 @@ from django.contrib import admin
 from .models import*
 admin.site.register(MedicalHistory)
 admin.site.register(Schedule)
+admin.site.register(BillInfo)
--- backend/adc/clinic/models.py
+++ backend/adc/clinic/models.py
@@ -1,3 +1,4 @@
+from telnetlib import STATUS
 from django.db import models
 from doctors.models import Doctors
 
@@ -22,4 +23,13 @@ class Schedule(models.Model):
     status = models.CharField(null=True, max_length=10, choices=STATUS)
     patient =  models.ForeignKey(Patient, null=True, on_delete= models.CASCADE)
     doctor = models.ForeignKey(Doctors, null=True, on_delete=models.CASCADE)
+
+
+class BillInfo(models.Model):
+    STATUS=(('Paid','Paid'),('Not Paid','Not Paid'))
+    patient =  models.ForeignKey(Patient, null=True, on_delete= models.CASCADE)
+    doctor = models.ForeignKey(Doctors, null=True, on_delete=models.CASCADE)
+    bdate=models.DateField()
+    payment=models.CharField(max_length=200,null=True)
+    status=models.CharField(null=True, max_length=10, choices=STATUS)
 
