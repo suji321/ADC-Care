059d277ffecefacd286e39b6c6527751957e4243
aleena-james <aleena.jms1099@gmail.com>
2022-04-09 13:30:29 +0530

    clinic form modified

--- adc/clinic/forms.py
+++ adc/clinic/forms.py
@@ -16,17 +16,17 @@ class PatientSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
 
-    def clean_phone(self):
-        phone = self.cleaned_data.get('phone')
-        if len(phone) is not 10:
-            raise forms.ValidationError('Phone number must have 10 digits')
-        return phone
+    # def clean_phone(self):
+    #     phone = self.cleaned_data.get('phone')
+    #     if len(phone) is not 10:
+    #         raise forms.ValidationError('Phone number must have 10 digits')
+    #     return phone
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if not validate_email(email, verify=True):
-            raise forms.ValidationError('Invalid email')
-        return email
+    # def clean_email(self):
+    #     email = self.cleaned_data.get('email')
+    #     if not validate_email(email, verify=True):
+    #         raise forms.ValidationError('Invalid email')
+    #     return email
     
     @transaction.atomic
     def save(self):
@@ -59,17 +59,17 @@ class DoctorSignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
 
-    def clean_phone(self):
-        phone = self.cleaned_data.get('phone')
-        if len(phone) is not 10:
-            raise forms.ValidationError('Phone number must have 10 digits')
-        return phone
+    # def clean_phone(self):
+    #     phone = self.cleaned_data.get('phone')
+    #     if len(phone) is not 10:
+    #         raise forms.ValidationError('Phone number must have 10 digits')
+    #     return phone
 
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        if not validate_email(email, verify=True):
-            raise forms.ValidationError('Invalid email')
-        return email
+    # def clean_email(self):
+    #     email = self.cleaned_data.get('email')
+    #     if not validate_email(email, verify=True):
+    #         raise forms.ValidationError('Invalid email')
+    #     return email
 
     @transaction.atomic
     def save(self):
